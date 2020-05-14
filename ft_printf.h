380eaab01bc038f8bedcd14300688fec0d6a3cf7
Kamil <kamiljanprzybyl@gmail.com>
2020-05-14 23:20:16 +0200

    structure added

--- ft_printf.h
+++ ft_printf.h
@@ -3,3 +3,23 @@
 #include <stdarg.h>
 
 int ft_printf(const char *restrict format, ...);
+
+typedef struct  s_info
+{
+    int             int_field;
+    unsigned int    unsigned_int_field;
+    double          double_field;
+    long long int   pointer_field;
+    unsigned char   char_field;
+    char            *res;
+
+    int is_width;
+    int is_precision;
+    int width_number;
+    int precision_number;
+    int is_pound_sign;
+    int is_zero_flag;
+    int is_plus_sign_flag;
+    int is_minus_sign_flag;
+
+}               t_info;
